a16acab0c66b8d73e53ad763455a25fbcfbf16b1
LAPTOP-GAMRIU19\Brady <bradyli@myyahoo.com>
2025-09-25 14:29:35 +1200

    Change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/https://github.com/brady-world2024/cosmic-portfolio.git/",
+  base: "/cosmic-portfolio/",
   plugins: [react()],
 });
